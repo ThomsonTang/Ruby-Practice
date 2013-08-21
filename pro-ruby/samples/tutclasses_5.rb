4b794d5bda35752c0db7afb65347df9c66a7ed17
Thomson Tang <guiketang@gmail.com>
2013-08-21 15:25:27 +0800

    add accessor method

--- pro-ruby/samples/tutclasses_5.rb
+++ pro-ruby/samples/tutclasses_5.rb
@@ -3,6 +3,12 @@ class BookInStock
         @isbn = isbn
         @price = Float(price)
     end
+    def isbn
+        @isbn
+    end
+    def price
+        @price
+    end
 end
 
 b1 = BookInStock.new("isbn1", 3)
@@ -10,4 +16,5 @@ puts b1
 b2 = BookInStock.new("isbn2", 3.14)
 puts b2
 b3 = BookInStock.new("isbn3", "5.14")
-puts b3
+puts "ISBN = #{b3.isbn}"
+puts "Price = #{b3.price}"

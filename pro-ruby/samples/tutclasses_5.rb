68cea43698d8008dbd87834c9f5d653be49027c1
Thomson Tang <guiketang@gmail.com>
2013-08-21 17:39:03 +0800

    add attr_reader shotcut

--- pro-ruby/samples/tutclasses_5.rb
+++ pro-ruby/samples/tutclasses_5.rb
@@ -1,4 +1,5 @@
 class BookInStock
+    attr_reader :isbn, :price
     def initialize(isbn, price)
         @isbn = isbn
         @price = Float(price)
@@ -8,9 +9,6 @@ class BookInStock
     end
 end
 
-b1 = BookInStock.new("isbn1", 3)
-puts b1
-b2 = BookInStock.new("isbn2", 3.14)
-puts b2
-b3 = BookInStock.new("isbn3", "5.14")
-puts b3
+book = BookInStock.new("isbn3", "5.14")
+puts "ISBN = #{book.isbn}"
+puts "Price = #{book.price}"
